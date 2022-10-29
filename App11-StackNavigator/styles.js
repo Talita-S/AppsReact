ff23f286c62c2b73e7a6feafa39875832e76c830
Talita-S <talitassantos2015@gmail.com>
2022-10-29 11:20:14 -0300

    projeto criado

---
+++ App11-StackNavigator/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create ({
+    centralizar: {
+        alignSelf: 'center',
+        padding: '80px',
+        flexDirection: 'row'
+    },
+   
+})
+
+export {styles}
