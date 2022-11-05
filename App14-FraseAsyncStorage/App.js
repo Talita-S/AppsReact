fb569400566b855856b0cb43379b9351d0eede1a
Talita-S <talitassantos2015@gmail.com>
2022-11-05 11:48:43 -0300

    projeto criado

---
+++ App14-FraseAsyncStorage/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { View, Text, Switch } from 'react-native';
+import { styles } from './styles';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dia: false,
+      pequeno: false
+    }
+  }
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text>Frases</Text>
+        <View style={styles.switch}>
+          <Text>Dia</Text>
+          <Switch
+            value={this.state.dia}
+            onValueChange={ (d) => this.setState({dia: d})}
+          />
+        </View>
+
+        <View style={styles.switch}>
+          <Text>Pequeno</Text>
+          <Switch
+            value={this.state.pequeno}
+            onValueChange={ (p) => this.setState({pequeno: p})}
+          />
+        </View>
+        
+        <View>
+          <Text>
+            "Não temos uma chance para fazer muitas coisas, então cada uma 
+            tem que ser excelente. Porque isso é a nossa vida. A vida é 
+            curta, e então você morre, você sabe? E todos nós escolhemos 
+            o que fazer das nossas vidas. Então é melhor que seja algo 
+            muito bom. É melhor que valha a pena". <br/>
+            (Steve Jobs)
+          </Text>
+        </View>
+        
+      </View>
+    )
+  }
+}
+
+export default App
---
+++ App14-FraseAsyncStorage/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native-web";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 0.3,
+    },
+    switch: {
+        flexDirection: 'row'
+    }
+})
+
+export {styles}
