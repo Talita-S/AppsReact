b5e35e2752d3fda341ebc5c9bbf237db99f88acb
Talita-S <talitassantos2015@gmail.com>
2022-11-18 16:13:32 -0300

    Card de citação criado e estilizado

--- App14-FraseAsyncStorage/App.js
+++ App14-FraseAsyncStorage/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StatusBar } from 'react-native';
 
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-import './styles.js';
 import { styles } from './styles.js';
 
 const App = () => {
     return(
-      <View style={styles.container}>
+      <View style={styles.container}> 
+
+        <StatusBar barStyle='light-content' />
+
         <View style={styles.card}>
+          <Text style={styles.title}>Quote of the Day</Text>
+          <FontAwesome5 name='quote-left' style={styles.quoteIcon}/>
+          <Text style={styles.quote}>It is the nature of the wise to resist pleasures, but the foolish to be a slave of them.</Text>
+          <FontAwesome5 name='quote-right' style={styles.quoteIconRight}/>
+          <Text style={styles.authorName}>— Author Name</Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.newQuote}>New quote</Text>
+          </TouchableOpacity>
+
+          <View style={styles.optionsContainer}>
+            <TouchableOpacity style={styles.optionButton}>
+              <FontAwesome name='volume-up' size={22} style={styles.optionIcon}/>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.optionButton}>
+              <FontAwesome5 name='copy' size={22} style={styles.optionIcon}/>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.optionButton}>
+              <FontAwesome name='twitter' size={22} style={styles.optionIcon}/>
+            </TouchableOpacity>
+          </View>
 
         </View>
       </View>
--- App14-FraseAsyncStorage/styles.js
+++ App14-FraseAsyncStorage/styles.js
@@ -5,13 +5,75 @@ const styles = StyleSheet.create({
         flex: '1', 
         justifyContent:'center', 
         alignItems:'center',
-        backgroundColor: '#54c7c3'
+        backgroundColor: '#4c94b5'
     },
     card: {
-        width: '90%',
+        width: '50%',
         backgroundColor: '#fff',
+        shadowColor: '#333837',
+        shadowOffset: {width: 10, height: 15},
+        shadowOpacity: 0.2,
         borderRadius: '20px',
         padding: '20px'
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: '22px',
+        fontWeight: '500',
+        color: '#333837',
+        marginBottom: '20px'
+    },
+    quote: {
+        fontSize: '16px',
+        lineHeight: '26px',
+        fontWeight: '400',
+        textAlign: 'center',
+        color: '#000',
+        marginBottom: '10px',
+        paddingHorizontal: '30px'
+    },
+    authorName: {
+        textAlign: 'right',
+        fontWeight: '300',
+        fontStyle: 'italic',
+        fontSiize: '16px',
+        color: '#000'
+    },
+    button: {
+        backgroundColor: '#4c94b5',
+        padding: '20px',
+        borderRadius: '30px',
+        marginVertical: '20px'
+    },
+    newQuote: {
+        color: '#fff',
+        fontSize: '18px',
+        textAlign: 'center'
+    },
+    quoteIcon: {
+        fontSize: '20px',
+        marginBottom: '-12px',
+        color: '#000'
+    },
+    quoteIconRight: {
+        fontSize: '20px',
+        color: '#000',
+        textAlign: 'right',
+        marginTop: '-20px',
+        marginBottom: '20px'
+    },
+    optionsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    optionIcon: {
+        color: '#4c94b5'
+    },
+    optionButton: {
+        borderWidth: '2px', 
+        borderColor: '#4c94b5',
+        borderRadius: '50px',
+        padding: '10px'
     }
 });
 
