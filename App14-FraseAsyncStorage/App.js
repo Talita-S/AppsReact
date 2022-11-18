a4c2d6191286618b403e605559e1c8f998bc8ce5
Talita-S <talitassantos2015@gmail.com>
2022-11-18 14:52:13 -0300

    Mudando a estrutura do projeto

--- App14-FraseAsyncStorage/App.js
+++ App14-FraseAsyncStorage/App.js
@@ -1,48 +1,15 @@
-import React, { Component } from 'react';
-import { View, Text, Switch } from 'react-native';
-import { styles } from './styles';
+import React from 'react';
+import { View, Text } from 'react-native';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dia: false,
-      pequeno: false
-    }
-  }
-  render() {
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+const App = () => {
     return(
-      <View style={styles.container}>
-        <Text style={styles.titulo}>Frases</Text>
-        <View style={styles.switch}>
-          <Text>Dia</Text>
-          <Switch
-            value={this.state.dia}
-            onValueChange={ (d) => this.setState({dia: d})}
-          />
-        </View>
-
-        <View style={styles.switch}>
-          <Text>Pequeno</Text>
-          <Switch
-            value={this.state.pequeno}
-            onValueChange={ (p) => this.setState({pequeno: p})}
-          />
-        </View>
-        
-        <View>
-          <Text>
-            "Não temos uma chance para fazer muitas coisas, então cada uma 
-            tem que ser excelente. Porque isso é a nossa vida. A vida é 
-            curta, e então você morre, você sabe? E todos nós escolhemos 
-            o que fazer das nossas vidas. Então é melhor que seja algo 
-            muito bom. É melhor que valha a pena". <br/>
-            (Steve Jobs)
-          </Text>
-        </View>
+      <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+        <Text>Citação</Text>
+        <FontAwesome5 name='quote-left'/>
       </View>
     )
-  }
 }
 
 export default App
