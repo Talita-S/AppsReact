3baf7ca07dcbcb5ba43ff93a1b2896d496e5bffc
Talita-S <talitassantos2015@gmail.com>
2022-11-18 14:58:55 -0300

    Criado e referenciado arquivo para estilização da página

--- App14-FraseAsyncStorage/App.js
+++ App14-FraseAsyncStorage/App.js
@@ -3,9 +3,12 @@ import { View, Text } from 'react-native';
 
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
+import './styles.js';
+import { styles } from './styles.js';
+
 const App = () => {
     return(
-      <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+      <View style={styles.container}>
         <Text>Citação</Text>
         <FontAwesome5 name='quote-left'/>
       </View>
